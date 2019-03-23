875e121138e7254b1b86f3d077853a8948cac4a0
Nick Smith <smithne@uw.edu>
2019-03-23 11:26:46 -0700

    Initial complete version.

--- etchasketch.js
+++ etchasketch.js
@@ -1,9 +1,48 @@
-let gridSize = Math.pow(16,16);
+const initialSize = 16;
+const gridWidth = 30; // in percent
 
-let gridContainer = document.querySelector('gameGrid');
+function initializeBoard(size) {
+    let squareSize = gridWidth/size; // in percent
+    let numSquares = size * size;
 
-for (let i = 0; i < gridSize; i++) {
-    let gridItem = document.createElement('div');
-    gridItem.classList.add('gridSquare');
-    gridItem.id = i;
+    let gridContainer = document.querySelector('#gameGrid');
+
+    gridContainer.style.gridTemplateRows = `${squareSize}vw `.repeat(size);
+    gridContainer.style.gridTemplateColumns = `${squareSize}vw `.repeat(size);
+    gridContainer.style.width = `${gridWidth}%`
+
+    for (let i = 0; i < numSquares; i++) {
+        let gridItem = document.createElement('div');
+        gridItem.classList.add('gridSquare');
+        gridItem.id = "square" + i;
+        gridContainer.append(gridItem);
+    }
+
+    let cells = document.querySelectorAll('.gridSquare');
+
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseover', shadeCell);
+    });
+    }
+
+function shadeCell(e) {
+    const cell = document.querySelector(`#${e.target.id}`);
+    cell.classList.add('shadedSquare');
 }
+
+function resetBoard() {
+    const newSize = prompt("Enter new grid size", "16");
+    const gridContainer = document.querySelector('#gameGrid');
+    // remove existing squares
+    while (gridContainer.hasChildNodes()) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+
+    initializeBoard(newSize);
+}
+
+const resetButton = document.querySelector("#resetBoard");
+
+resetButton.addEventListener("click", resetBoard);
+
+initializeBoard(initialSize);
